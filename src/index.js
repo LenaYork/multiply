f1536c676355f6eb088eef28240ef8d9dfe81c07
LenaYork <lenayork@tut.by>
2019-10-12 20:48:11 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function multiply(first, second) {
-  // your solution
+ 
+  // let result = "";   //первые 3
+  // result += first*second;
+  // return result;
+
+  let magicalLetter = "n";
+  first += first+magicalLetter;
+  first = +first;
+  second += second+magicalLetter;
+  second = +second;
+  result =+ first * second;
+  return result;
+  
 }
